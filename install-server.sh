52447ba3453b0afe50702a63aa64b72f2b1cc529
mswlandi <fakepsnmarcos@gmail.com>
2022-07-14 19:32:20 +0200

    update server version to 1.19.10.03

--- install-server.sh
+++ install-server.sh
@@ -1,5 +1,5 @@
 # this is supposed to be run on a fresh ubuntu machine
 apt-get install -y unzip screen
-wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.2.02.zip
-unzip bedrock-server-1.19.2.02.zip -d server
+wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.10.03.zip
+unzip bedrock-server-1.19.10.03.zip -d server
 ufw allow 19132/tcp
--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,3 @@
 # this is supposed to be run after install-server.sh
+cd server
 LD_LIBRARY_PATH=. ./bedrock_server
